37027d4e771fc4171d948bc320f729f668275f1d
Samuel <05334@duque.br.local>
2025-08-25 21:11:43 -0300

    Criação da lígica de cálculo

--- Aula04/src/main/java/com/samuel/aula04/Aula04.java
+++ Aula04/src/main/java/com/samuel/aula04/Aula04.java
@@ -11,6 +11,9 @@ package com.samuel.aula04;
 public class Aula04 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JFTela app = new JFTela ();
+        app.setTitle("GITHUB");
+        app.setLocationRelativeTo(app);
+        app.setVisible(true);
     }
 }
